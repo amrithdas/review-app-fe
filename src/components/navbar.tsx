3c4ec7261d1eb3aee3b955d42aa7bc6cd5890e1d
Amrithadas P Anil <amrithadas@gmail.com>
2024-06-01 14:23:40 +0530

    login and signup implementation

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
+import useAuth from '../hooks/useAuth';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 interface NavbarProps {
   onSignupClick: () => void;
-  
+  onLoginClick: () => void;
 }
 
-const Navbar: React.FC<NavbarProps> = ({ onSignupClick }) => {
+const Navbar: React.FC<NavbarProps> = ({ onSignupClick, onLoginClick }) => {
+
+  const isAuthenticated = useAuth();
+  console.log(isAuthenticated)
+
+  const handleLogout = async () => {
+    try {
+      
+      const headers = {
+        'X-CSRFToken': Cookies.get('csrftoken')
+      };
+
+      const response = await axios.post('http://127.0.0.1:8000/accounts/logout/', {}, {
+          headers: headers,
+          withCredentials: true  // Ensure credentials are included
+      });
+      
+      if (response.status === 200) {
+          // Clear any user-related state
+          // setUser(null);
+          // Redirect or refresh the page
+          window.location.reload();
+      }
+  } catch (error) {
+      console.error('An error occurred during logout:', error);
+  }
+  }
+
   return (
     <nav>
       <div className="fixed top-0 left-0 right-0 z-10 max-w-full mx-auto px-4 sm:px-6 lg:px-8">
@@ -17,15 +47,23 @@ const Navbar: React.FC<NavbarProps> = ({ onSignupClick }) => {
           <a href="/signup" className="text-white text-sm mr-4">
               Write a Review
             </a>
-            <a href="/login" className="text-white hover:bg-white hover:text-black px-4 py-2 border-solid border-2 border-white rounded-md text-sm font-medium">
-              Login
-            </a>
-            <button
-              onClick={onSignupClick}
-              className="ml-4 text-white bg-blue-500 hover:bg-blue-700 px-3 py-2  rounded-md text-sm font-medium"
+            {!isAuthenticated?
+            <><button
+                onClick={onLoginClick}
+                className="text-white hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Login
+              </button><button
+                onClick={onSignupClick}
+                className="ml-4 text-white bg-blue-500 hover:bg-blue-700 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                  Signup
+                </button></>:<button
+                onClick={handleLogout}
+              className="text-white hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
             >
-              Signup
-            </button>
+              Logout
+            </button>}
           </div>
         </div>
       </div>
